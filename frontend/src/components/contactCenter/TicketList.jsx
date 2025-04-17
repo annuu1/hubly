7526270dc0947183a39cf765c2785d64313c354f
annuu1 <mannusng876@gmail.com>
2025-04-17 23:36:52 +0530

    style: enhance TicketList component with improved layout and avatar integration

--- frontend/src/components/contactCenter/TicketList.jsx
+++ frontend/src/components/contactCenter/TicketList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import styles from './TicketList.module.css';
+import avatar from '../../assets/icons/avatar.png';
 
 const TicketList = ({ tickets, onSelect }) => {
     console.log(tickets[0])
     return (
-        <ul>
+        <ul className={styles.ticketList}>
+            {tickets.length === 0 && <li>No tickets available</li>}
             {tickets.map((ticket, index) => (
-                <li key={ticket.ticketId} onClick={() => onSelect(ticket)}>
+                <li key={ticket.ticketId} onClick={() => onSelect(ticket)} className={styles.ticketItem}>
+                    <img src={avatar} alt="Avatar" className={styles.avatar} />
                     Chat {index+1}
                 </li>
             ))}
